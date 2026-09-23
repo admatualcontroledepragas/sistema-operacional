92d1188015c7079343c684cd09312a788ae5f1dd
Caio <contato.atualsaudeambiental@gmail.com>
2026-09-22 21:40:09 -0300

    Add files via upload

---
+++ sw.js.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'portal-atual-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './comunicados/index.html',
+  './relatorios/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna do cache se encontrar, senão busca na rede
+        return response || fetch(event.request);
+      })
+  );
+});
