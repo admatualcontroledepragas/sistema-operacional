8c1bbb4ac9f7dddfb5729f5f687f1bdf6b97313e
Caio <contato.atualsaudeambiental@gmail.com>
2026-09-24 14:24:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portal-atual-v6'; // Versão atualizada com os ficheiros separados
+const CACHE_NAME = 'portal-atual-v7'; // Versão atualizada com os ficheiros separados
 
 const urlsToCache = [
   './',
